948a66e4b293f4b6cf867bbcb7880e63e4bf1a2a
Felipe Mejias <felipe.mejias50@gmail.com>
2022-05-29 16:34:09 -0300

    xadrez movimentos

--- index.js
+++ index.js
@@ -152,31 +152,32 @@ app.post('/partidas',async(req,res)=>{
         {peca:'p1a',pos:[7,1],numero:0},{peca:'p2a',pos:[7,2],numero:1},{peca:'p3a',pos:[7,3],numero:2},{peca:'p4a',pos:[7,4],numero:3},
         {peca:'p5a',pos:[7,5],numero:4},{peca:'p6a',pos:[7,6],numero:5},{peca:'p7a',pos:[7,7],numero:6},{peca:'p8a',pos:[7,8],numero:7},
 
-        {peca:'t1a',pos:[8,1],numero:8},{peca:'t2a',pos:[8,8],numero:9},{peca:'c1a',pos:[8,3],numero:10},{peca:'c2a',pos:[8,6],numero:11},
-        {peca:'b1a',pos:[8,2],numero:12},{peca:'b2a',pos:[8,7],numero:13},{peca:'r1a',pos:[8,5],numero:14},{peca:'r2a',pos:[8,4],numero:15},
+        {peca:'t1a',pos:[8,1],numero:8},{peca:'t2a',pos:[8,8],numero:9},{peca:'c1a',pos:[8,2],numero:10},{peca:'c2a',pos:[8,7],numero:11},
+        {peca:'b1a',pos:[8,3],numero:12},{peca:'b2a',pos:[8,6],numero:13},{peca:'r1a',pos:[8,5],numero:14},{peca:'x1a',pos:[8,4],numero:15},
     
         {peca:'p1b',pos:[2,1],numero:16},{peca:'p2b',pos:[2,2],numero:17},{peca:'p3b',pos:[2,3],numero:18},{peca:'p4b',pos:[2,4],numero:19},
         {peca:'p5b',pos:[2,5],numero:20},{peca:'p6b',pos:[2,6],numero:21},{peca:'p7b',pos:[2,7],numero:22},{peca:'p8b',pos:[2,8],numero:23},
 
-        {peca:'t1b',pos:[1,1],numero:24},{peca:'t2b',pos:[1,8],numero:25},{peca:'c1b',pos:[1,3],numero:26},{peca:'c2b',pos:[1,6],numero:27},
-        {peca:'b1b',pos:[1,2],numero:28},{peca:'b2b',pos:[1,7],numero:29},{peca:'r1b',pos:[1,5],numero:30},{peca:'r2b',pos:[1,4],numero:31}
+        {peca:'t1b',pos:[1,1],numero:24},{peca:'t2b',pos:[1,8],numero:25},{peca:'c1b',pos:[1,2],numero:26},{peca:'c2b',pos:[1,7],numero:27},
+        {peca:'b1b',pos:[1,3],numero:28},{peca:'b2b',pos:[1,6],numero:29},{peca:'r1b',pos:[1,5],numero:30},{peca:'x1b',pos:[1,4],numero:31}
     ]
 
     try{
-        await db.collection('partidas').insertOne({ja:nome,jb:amigo,todas,id:nome+amigo})
-        res.send(200)
+        await db.collection('partidas').insertOne({ja:nome,jb:amigo,todas:todas,id:nome+amigo})
+        res.sendStatus(200)
     }catch{res.sendStatus(499)}
     
 })
 app.get('/partidas',async(req,res)=>{
     const {nome}=req.headers
-    try{
+    
         let partida=await db.collection('partidas').findOne({ja:nome})
         if(!partida){partida=await db.collection('partidas').findOne({jb:nome})}
-        if(!partida){res.sendStatus(499)}
-        res.send(partida.todas)
-    }catch{res.sendStatus(499)}
+        if(partida && partida!=null){res.send(partida.todas)}
+        else{res.sendStatus(499)}
 })
+
+        
 
 function distancia(a,b){
     if(a>b){return a-b}
@@ -195,7 +196,7 @@ function come(l,c,player,todas){
             if(todas[k].peca[2]==player){return false}
             if(todas[k].peca[2]!=player){return todas[k].numero}
         }
-    }return true
+    }return 32
 }
 function pode(qual,inicio,onde,todas,player){
     if(/*peao A*/ 
@@ -204,7 +205,7 @@ function pode(qual,inicio,onde,todas,player){
                 if(temAlg(onde[0],onde[1],todas)){return false}
                 if(inicio[0]==7){if(onde[1]==inicio[1] &&onde[1]==inicio[1]-2 ){if(temAlg(3,inicio[1]-1,todas)){return false}return true}}
                 if( onde[1]==inicio[1]&&onde[0]==inicio[0]-1){return true}
-                if( distancia(onde[0],inicio[0]) == distancia(onde[1],inicio[1]) &&distancia(onde[1],inicio[1])==1&& onde[0]<inicio[0] && come(onde[0],onde[1],player,todas)){return true}
+                if( distancia(onde[0],inicio[0]) == distancia(onde[1],inicio[1]) &&distancia(onde[1],inicio[1])==1&& onde[0]<inicio[0] && come(onde[0],onde[1],player,todas) && come(onde[0],onde[1],player,todas)<32){return true}
             }
 
             if(/*peao b*/ 
@@ -213,7 +214,7 @@ function pode(qual,inicio,onde,todas,player){
                 if(temAlg(onde[0],onde[1],todas)){return false}
                 if(inicio[0]==2){if(onde[1]==inicio[1] &&onde[1]==inicio[1]+2 ){if(temAlg(6,inicio[1]+1,todas)){return false}return true}}
                 if( onde[1]==inicio[1]&&onde[0]==inicio[0]+1){return true}
-                if( distancia(onde[0],inicio[0]) == distancia(onde[1],inicio[1]) &&distancia(onde[1],inicio[1])==1&& onde[0]>inicio[0] && come(onde[0],onde[1],player,todas)){return true}
+                if( distancia(onde[0],inicio[0]) == distancia(onde[1],inicio[1]) &&distancia(onde[1],inicio[1])==1&& onde[0]>inicio[0] && come(onde[0],onde[1],player,todas) && come(onde[0],onde[1],player,todas)<32){return true}
             }
 
 
@@ -250,7 +251,7 @@ function pode(qual,inicio,onde,todas,player){
 
             if(/*bispo*/ 
                 qual== 12 || qual== 13 || qual== 28 || qual==  29 
-            ){
+            ){let l;let c;
                 if( onde[0]==inicio[0] && onde[1]==inicio[1] ){return false}
                 if( distancia(onde[0],inicio[0]) == distancia(onde[1],inicio[1]) ){
                     l=inicio[0];c=inicio[1];let addl=1;let addc=1;if(inicio[0]>onde[0]){addl=-1};if(inicio[1]>onde[1]){addc=-1}
@@ -314,17 +315,22 @@ app.put('/partidas',async(req,res)=>{
     try{
         let partida=await db.collection('partidas').findOne({ja:nome});player='a'
         if(!partida){partida=await db.collection('partidas').findOne({jb:nome});player='b'}
-            const {todas}=partida
-            const inicio=todas[qual].pos
+            const todas=partida.todas
+            const inicio=[todas[qual].pos[0],todas[qual].pos[1]]
             if(!pode(qual,inicio,onde,todas,player)){res.sendStatus(444) }
-            if(!come(onde[0],onde[1],player,todas)){res.sendStatus(443) }
-            if(come(onde[0],onde[1],player,todas)){
+             else if(!come(onde[0],onde[1],player,todas)){res.sendStatus(443) }
+            else{ 
+                console.log('aqui eu')
+                if(come(onde[0],onde[1],player,todas)!=32){
+                    console.log(come(onde[0],onde[1],player,todas))
                 todas[come(onde[0],onde[1],player,todas)].pos=[0,0]
                 await db.collection('partidas').updateOne({partida},{$set:{todas}})
             }
-            todas[qual].pos=onde
-            await db.collection('partidas').updateOne({partida},{$set:{todas}})
-        res.sendStatus(200)    
+            todas[qual].pos=[onde[0],onde[1]]
+            console.log(todas[qual].pos)
+            if(player=='a')await db.collection('partidas').updateOne({ja:nome},{$set:{todas:todas}})
+            if(player=='b')await db.collection('partidas').updateOne({jb:nome},{$set:{todas:todas}})
+        res.sendStatus(200)    }
     }catch(e){res.sendStatus(500);console.log(e)}
 })
 
@@ -334,4 +340,4 @@ app.put('/partidas',async(req,res)=>{
 
 
 
-app.listen(port,()=>console.log(`servidor em pé na porta ${port}`))
+app.listen(5743,()=>console.log(`servidor em pé na porta ${port}`))
